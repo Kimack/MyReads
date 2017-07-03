002c1f988aa8a253789e46174fd906f895c542c5
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-07-04 01:44:24 +0200

    Refactor ListBooks

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import startCase from 'lodash.startcase';
+// our modules
 import Book from './Book';
 
-class ListBooks extends Component {
-  render() {
-    const shelves = {
-      currentlyReading: 'Currently Reading',
-      wantToRead: 'Want To Read',
-      read: 'read'
-    };
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="open-search">
-          <Link to='/search' alt='Add books'>Add Books</Link>
-        </div>
-        {Object.keys(shelves).map((shelf) => {
-          return (
-            <div key={shelf} className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">{shelves[shelf]}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.props.books
-                        .filter(book => book.shelf === shelf)
-                        .map(book => (
-                          <Book
-                            key={book.id}
-                            book={book}
-                            handleBookListChange={this.props.handleBookListChange}
-                          />
-                        ))
-                      }
-                    </ol>
-                  </div>
+function ListBooks(props) {
+  const shelves = ['currentlyReading', 'wantToRead', 'read'];
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="open-search">
+        <Link to='/search' alt='Add books'>Add Books</Link>
+      </div>
+      {shelves.map((shelf) => {
+        return (
+          <div key={shelf} className="list-books-content">
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">{startCase(shelf)}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {props.books
+                      .filter(book => book.shelf === shelf)
+                      .map(book => (
+                        <Book
+                          key={book.id}
+                          book={book}
+                          handleBookListChange={props.handleBookListChange}
+                        />
+                      ))
+                    }
+                  </ol>
                 </div>
               </div>
             </div>
-          )
-        })}
-      </div>
-    )
-  }
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default ListBooks;
