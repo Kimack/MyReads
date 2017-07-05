f46437d6e240f822c0ece4a5babf4c88888d40cc
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-07-05 04:17:26 +0200

    Add /details route and refactor

--- src/App.js
+++ src/App.js
@@ -3,28 +3,56 @@ import { Route } from 'react-router-dom';
 // our modules
 import * as BooksAPI from './BooksAPI';
 import './App.css';
+// components
 import ListBooks from './ListBooks';
 import Search from './Search';
+import Details from './Details';
 
 class BooksApp extends React.Component {
-  state = {
-    books: []
+  constructor(props) {
+    super(props)
+    this.getBook = this.getBook.bind(this)
+    this.findBookInList = this.findBookInList.bind(this)
+    this.handleBookListChange = this.handleBookListChange.bind(this)
+
+    this.state = {
+      books: []
+    }
   }
 
-  // Returns the index of a book that's already in a list,
-  // or -1 if it's not in a list.
-  findBookInList = (id) => {
+  findBookInList(id) {
     return this.state.books.findIndex(book => book.id === id)
   }
 
-  getAllBooks = () => {
+  getBook(id) {
+    const bookIdx = this.findBookInList(id);
+    if (bookIdx !== -1) {
+      return Promise.resolve(this.state.books[bookIdx]);
+    }
+    // Because the API returns new books with a random shelf assigned, visiting
+    // the /details/:id page of a book without loading the books first, will
+    // mean that we don't know the real state of the book. At this point we're
+    // not going to jump through hoops because of inconsistent DB design, so
+    // the shelf is set to 'none' by default.
+    return BooksAPI.get(id)
+      .then((book) => {
+        book.shelf = 'none';
+        this.setState(state => {
+          state.books.push(book);
+          return state;
+        })
+        return book;
+      })
+  }
+
+  getAllBooks() {
     BooksAPI.getAll()
       .then((books) => {
         this.setState({books})
       })
   }
 
-  handleBookListChange = (book, shelf) => {
+  handleBookListChange(book, shelf) {
     BooksAPI.update({id: book.id}, shelf)
       .then((res) => {
         const idx = this.findBookInList(book.id);
@@ -42,13 +70,6 @@ class BooksApp extends React.Component {
           })
         }
     })
-  }
-
-  getBook = (id) => {
-    BooksAPI.get(id)
-      .then(book => {
-        this.setState(state => Object.assign(state, book))
-      })
   }
 
   componentDidMount() {
@@ -69,6 +90,13 @@ class BooksApp extends React.Component {
             books={this.state.books}
             handleBookListChange={this.handleBookListChange}
             findBookInList={this.findBookInList}
+          />
+        )} />
+        <Route path='/details/:id' render={({match, location}) => (
+          <Details
+            getBook={this.getBook}
+            bookId={match.params.id}
+            handleBookListChange={this.handleBookListChange}
           />
         )} />
       </div>
