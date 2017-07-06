6f77498e92efea34c43c0d9c71ec29a805f54042
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-07-06 07:37:26 +0200

    Format and refactor

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import startCase from 'lodash.startcase';
+import React from "react";
+import { Link } from "react-router-dom";
+import startCase from "lodash.startcase";
 // our modules
-import Book from './Book';
+import Book from "./Book";
 
 function ListBooks(props) {
-  const shelves = ['currentlyReading', 'wantToRead', 'read'];
+  const shelves = ["currentlyReading", "wantToRead", "read"];
   return (
     <div className="list-books">
       <div className="list-books-title">
         <h1>MyReads</h1>
       </div>
       <div className="open-search">
-        <Link to="/search" alt="Add books">Add Books</Link>
+        <Link to="/search" alt="Add books">
+          Add Books
+        </Link>
       </div>
-      {shelves.map((shelf) => {
+      {shelves.map(shelf => {
         return (
           <div key={shelf} className="list-books-content">
             <div>
               <div className="bookshelf">
-                <h2 className="bookshelf-title">{startCase(shelf)}</h2>
+                <h2 className="bookshelf-title">
+                  {startCase(shelf)}
+                </h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
                     {props.books
                       .filter(book => book.shelf === shelf)
-                      .map(book => (
+                      .map(book =>
                         <Book
                           key={book.id}
                           book={book}
                           handleBookListChange={props.handleBookListChange}
                         />
-                      ))
-                    }
+                      )}
                   </ol>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default ListBooks;
--- src/Search.js
+++ src/Search.js
@@ -1,74 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import queryString from 'query-string';
-import * as BooksAPI from './BooksAPI';
+import React from "react";
+import { Link } from "react-router-dom";
+import queryString from "query-string";
+import * as BooksAPI from "./BooksAPI";
 // our modules
-import Book from './Book';
+import Book from "./Book";
 
 class Search extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       books: [],
-      query: ''
-    }
-    this.searchDelay = 500
+      query: ""
+    };
+    this.searchDelay = 500;
   }
 
   resetBookList() {
-    this.setState({books: []})
+    this.setState({ books: [] });
   }
 
   componentWillMount() {
     const query = queryString.parse(location.search);
     if (query.search) {
-      this.handleSearchInput(query.search)
+      this.handleSearchInput(query.search);
     }
   }
 
   handleSearchInput(searchQuery) {
     const queryCopy = searchQuery;
-    this.setState(state => state.query = searchQuery)
+    this.setState(state => (state.query = searchQuery));
 
     setTimeout(() => {
-      if (queryCopy === '') {
+      if (queryCopy === "") {
         this.resetBookList();
         return;
       }
       // Only if the query is unchanged after 500 ms, do we do a search.
       if (this.state.query === queryCopy) {
-        BooksAPI.search(this.state.query)
-          .then(books => {
-            if (books.error) {
-              this.resetBookList();
-              return;
-            }
-            if (books.length) {
-              this.setState(state => {
-                state.books = books.map(book => {
-                  const idx = this.props.findBookInList(book.id)
-                  return idx !== -1 ? this.props.books[idx] :
-                                      Object.assign(book, {shelf: 'none'})
-                })
-                return state;
-              })
-            }
-          })
+        BooksAPI.search(this.state.query).then(books => {
+          if (books.error) {
+            this.resetBookList();
+            return;
+          }
+          if (books.length) {
+            this.setState(state => {
+              state.books = books.map(book => {
+                const idx = this.props.findBookInList(book.id);
+                return idx !== -1
+                  ? this.props.books[idx]
+                  : Object.assign(book, { shelf: "none" });
+              });
+              return state;
+            });
+          }
+        });
       }
-    }, this.searchDelay)
+    }, this.searchDelay);
   }
 
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to="/" alt="Close search" className="close-search">Close</Link>
+          <Link to="/" alt="Close search" className="close-search">
+            Close
+          </Link>
           <div className="search-books-input-wrapper">
             <input
               type="text"
               placeholder="Search by title or author"
-              onChange={(e) => this.handleSearchInput(e.target.value)}
+              onChange={e => this.handleSearchInput(e.target.value)}
               value={this.state.query}
               autoFocus
             />
@@ -76,19 +78,17 @@ class Search extends React.Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.books
-              .map(book => (
-                <Book
-                  key={book.id}
-                  book={book}
-                  handleBookListChange={this.props.handleBookListChange}
-                />
-              ))
-            }
+            {this.state.books.map(book =>
+              <Book
+                key={book.id}
+                book={book}
+                handleBookListChange={this.props.handleBookListChange}
+              />
+            )}
           </ol>
         </div>
       </div>
-    )
+    );
   }
 }
 
