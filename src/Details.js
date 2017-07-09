417156287d1d7139a132a51d2f177f5834ab54cf
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-07-09 04:34:47 +0200

    Fix 'scroll to top on navigation' site-wide

--- src/Details.js
+++ src/Details.js
@@ -8,9 +8,7 @@ class Details extends React.Component {
     book: placeholder
   };
 
-  // Get the book from the App state or from the API.
   componentDidMount() {
-    window.scrollTo(0, 0);
     const { getBook, bookId } = this.props;
     getBook(bookId).then(book => {
       this.setState({ book });
